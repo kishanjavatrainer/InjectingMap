fdcb662a6dd5d7392df7709e66f87ae2a7f660b9
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-07 19:07:36 +0530

    InjectingMap source code

---
+++ InjectingMap/src/com/infotech/clientTest/Test.java
@@ -0,0 +1,46 @@
+package com.infotech.clientTest;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.infotech.info.CompanyInfo;
+import com.infotech.model.Student;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		CompanyInfo companyInfo = ctx.getBean("companyInfo", CompanyInfo.class);
+		
+		Map<Integer, String> empIdAndNameMap = companyInfo.getEmpIdAndNameMap();
+		
+		Set<Entry<Integer, String>> entrySet = empIdAndNameMap.entrySet();
+		for (Entry<Integer, String> entry : entrySet) {
+			System.out.println(entry.getKey()+"\t"+entry.getValue());
+		}
+		
+		System.out.println("------------------------------------------------");
+		
+		Map<Integer, Student> studentIdMap = companyInfo.getStudentIdMap();
+		Set<Entry<Integer, Student>> entrySet2 = studentIdMap.entrySet();
+		for (Entry<Integer, Student> entry : entrySet2) {
+			
+			System.out.println(entry.getKey());
+			Student student = entry.getValue();
+			
+			System.out.println(student.getStudentId()+"\t"+student.getStudentName());
+			
+		}
+		((AbstractApplicationContext) ctx).close();
+	}
+	
+	
+
+}
